be19fc7b53ab87941f1af80db2613f1bcdafbf57
lily <614720105@qq.com>
2020-04-29 20:43:26 +0800

    content:AnimatedWidget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
 
+//======================================StatefulWidget=================================//
 class LogoApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -9,35 +10,29 @@ class LogoApp extends StatefulWidget {
   }
 }
 
-class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin{
- Animation<double> animation;
- AnimationController controller;
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
 
- @override
+  @override
   void initState() {
     super.initState();
     controller = new AnimationController(
-        duration: const Duration(
-          microseconds: 2000),
-        vsync: this);
-    animation = new Tween(begin: 0.0,end: 300.0).animate(controller)
-    ..addListener(() {
-      setState(() {
-
-      });
-    });
+        duration: const Duration(microseconds: 2000), vsync: this);
+    animation = new Tween(begin: 0.0, end: 300.0).animate(controller);
+//    ..addListener(() {
+//      setState(() {
+//
+//      });
+//    });
     controller.forward();
   }
+
   @override
   Widget build(BuildContext context) {
-   return new Center(
-     child: new Container(
-       margin: new EdgeInsets.symmetric(vertical: 10.0),
-       height: animation.value,
-       width: animation.value,
-       child: new FlutterLogo(),
-     ),
-   );
+    return new AnimatedLogo(
+      animation: animation,
+    );
     throw UnimplementedError();
   }
 
@@ -46,8 +41,27 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin{
     controller.dispose();
     super.dispose();
   }
-
 }
+
+//======================================AnimatedWidget=================================//
+class AnimatedLogo extends AnimatedWidget {
+  AnimatedLogo({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
+    return new Center(
+      child: new Container(
+        margin: new EdgeInsets.symmetric(vertical: 10.0),
+        height: animation.value,
+        width: animation.value,
+        child: new FlutterLogo(),
+      ),
+    );
+    throw UnimplementedError();
+  }
+}
+
 void main() {
   runApp(new LogoApp());
 }
